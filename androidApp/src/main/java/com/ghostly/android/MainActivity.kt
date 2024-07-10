17ce267f85d3ef7863ce291b1782699a19bdffd0
roadrage312 <roadrage318@gmail.com>
2024-07-10 23:00:57 +0530

    Add basic scaffolding (#3)

--- androidApp/src/main/java/com/ghostly/android/MainActivity.kt
+++ androidApp/src/main/java/com/ghostly/android/MainActivity.kt
@@ -3,13 +3,22 @@ package com.ghostly.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.ghostly.Greeting
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.ghostly.android.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,18 +28,39 @@ class MainActivity : ComponentActivity() {
             AppTheme(
                 darkTheme = false
             ) {
-                GreetingView(Greeting().greet())
+                val navController = rememberNavController()
+                AppNavigation(navController = navController)
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingView(text: String) {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = text)
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Image(
+                        modifier = Modifier.size(40.dp),
+                        painter = painterResource(id = R.drawable.ic_ghost_logo),
+                        contentDescription = "Ghost Logo",
+                        contentScale = ContentScale.Inside
+                    )
+                }
+            )
+        }
+    ) { padding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            color = MaterialTheme.colorScheme.background
+        ) {
+
+        }
     }
 }
 
@@ -38,6 +68,6 @@ fun GreetingView(text: String) {
 @Composable
 fun DefaultPreview() {
     AppTheme {
-        GreetingView(Greeting().greet())
+        MainScreen()
     }
 }
